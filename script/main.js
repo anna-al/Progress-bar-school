aac1851828ed28bedcee90d723832951c765c92e
Anna lisitsyna <anna@annalis.ru>
2020-11-11 20:26:14 +0300

    работа с коллекцией, циклом  и условием

--- script/main.js
+++ script/main.js
@@ -1,7 +1,14 @@
 const ProgressBarItem = document.querySelector('.ProgressBar');
 
-const PredmetItem = document.querySelector('.Predmet');
-PredmetItem.classList.add('ProgressBar_background-color_yellow');
+const PredmetItem = document.querySelectorAll('.Predmet');
+for (const Predmet of PredmetItem) {
+    if (Predmet.classList.contains('ProgressBar_background-color_yellow')) {
+        Predmet.classList.remove('ProgressBar_background-color_yellow');  
+    } else {
+        Predmet.classList.add('ProgressBar_background-color_yellow');  
+    }
+}
+//PredmetItem.classList.toggle('ProgressBar_background-color_yellow');
 
 console.log(ProgressBarItem);
 console.log(PredmetItem);
