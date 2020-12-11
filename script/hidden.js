5eb411400dd10e7085410d11ec283cf3195ed746
Anna lisitsyna <anna@annalis.ru>
2020-12-12 00:12:49 +0300

    вделала выбор по классу и имени

--- script/hidden.js
+++ script/hidden.js
@@ -5,25 +5,39 @@ const selectedChild = document.querySelector('.childSelect');
 const childSelect1 = document.querySelector('.'+childName1);
 const childSelect2 = document.querySelector('.'+childName2);
 
+const klassItems = document.querySelectorAll('.klass');
+const selectedKlass = document.querySelector('.klassSelect');
+
+
 console.log('1  '+ selectedChild.value);
 console.log('11  ');
 console.log(childSelect1);
-console.log('12  ');
-console.log(childSelect2);
+console.log(klassItems);
+
 
 selectedChild.onchange = function () {
-    console.log("2  "+selectedChild.value);
     if (selectedChild.value === childName1) {
-        console.log("3  "+selectedChild.value);
         childSelect1.classList.remove('hidden');
         childSelect2.classList.add('hidden');
     }
-
     if (selectedChild.value === childName2) {
-        console.log("4  "+selectedChild.value);
         childSelect2.classList.remove('hidden');
         childSelect1.classList.add('hidden');
     
     }
 }
 
+selectedKlass.onchange = function () {
+    console.log("2  ");    
+    console.log("2  "+selectedKlass.value);
+    for (const klass of klassItems) {
+        console.log(klass);
+        klass.classList.add('hidden');
+    }
+    const visibleKlasses = document.querySelectorAll('.'+selectedKlass.value);
+    for (const visibleKlass of visibleKlasses) {
+        console.log('');
+        console.log(visibleKlass);
+        visibleKlass.classList.remove('hidden');
+    }
+}
